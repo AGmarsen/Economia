37f6652f617673b508878840095dcee9c1dc241d
AGmarsen <thorclausen123@gmail.com>
2026-05-18 13:09:35 +0200

    interests

--- src/app/accountantService.ts
+++ src/app/accountantService.ts
@@ -20,6 +20,15 @@ export class AccountantService {
   private _mortgageLoan = signal<number>(0);
   mortgageLoan = this._mortgageLoan.asReadonly();
 
+  private _bankLoanInterestRate = signal<number>(0);
+  bankLoanInterestRate = this._bankLoanInterestRate.asReadonly();
+
+  private _mortgageLoanInterestRate = signal<number>(0);
+  mortgageLoanInterestRate = this._mortgageLoanInterestRate.asReadonly();
+
+  private _loanTermYears = signal<number>(0);
+  loanTermYears = this._loanTermYears.asReadonly();
+
   setDownPayment(downPayment: number) {
     this.downPayment = this.roundToCurrency(downPayment);
     this.calculateLoan();
@@ -31,9 +40,21 @@ export class AccountantService {
   }
 
   calculateLoan() {
-    this._loanAmount.set(this.investmentSum - this.downPayment);
+    this._loanAmount.set(this.roundToCurrency(this.investmentSum - this.downPayment));
     this._mortgageLoan.set(Math.min(this._loanAmount(), this.roundToCurrency(this.investmentSum * this._mortgageCover())));
-    this._bankLoan.set(this._loanAmount() - this._mortgageLoan());
+    this._bankLoan.set(this.roundToCurrency(this._loanAmount() - this._mortgageLoan()));
+  }
+
+  setBankLoanInterestRate(rate: number) {
+    this._bankLoanInterestRate.set(rate);
+  }
+
+  setMortgageLoanInterestRate(rate: number) {
+    this._mortgageLoanInterestRate.set(rate);
+  }
+
+  setLoanTerm(term: number) {
+    this._loanTermYears.set(term);
   }
 
 
